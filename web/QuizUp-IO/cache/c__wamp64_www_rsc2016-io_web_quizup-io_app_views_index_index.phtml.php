379903283b470107fedf6a3e0ca0800aefa3ec18
= <[ivamilicevi@foi.hr]>
2016-11-26 16:32:42 +0100

    Homepage refactored

--- web/QuizUp-IO/cache/c__wamp64_www_rsc2016-io_web_quizup-io_app_views_index_index.phtml.php
+++ web/QuizUp-IO/cache/c__wamp64_www_rsc2016-io_web_quizup-io_app_views_index_index.phtml.php
@@ -23,30 +23,30 @@
   </head>
   <body>
     <nav class="navbar navbar-default">
-      <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <i class="fa fa-bars"></i>
-          </button>
-          <a class="navbar-brand" href="#">QuizUp</a>
-        </div>
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <i class="fa fa-bars"></i>
+      </button>
+      <a class="navbar-brand" href="#">QuizUp</a>
+    </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Events</a></li>
-            <li><a href="#">History</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="">Login</a></li>
-          </ul>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="<?= $this->url->get('index') ?>">Home</a></li>
+        <li><a href="#">Events</a></li>
+        <li><a href="#">History</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/login">Login</a></li>
+      </ul>
 
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 
     <section class="info-section">
         <div class="tcell">
@@ -58,53 +58,53 @@
     </section>
 
     <section class="events">
-      <h1>Nearby Events</h1>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-4">
-            <div class="event-item">
-              <h3>Event #1</h3>
-              <div class="event-description">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis veritatis maiores perferendis quas sequi magnam corporis exercitationem natus sint ullam.</p>
-              </div>
-              <div class="event-location"><i class="fa fa-map-marker"></i> <strong>Location: </strong> Pivnica Medvedgrad</div>
-              <div class="event-date"><i class="fa fa-calendar"></i><strong> Date: </strong> 20.12.2017.</div>
-              <div class="event-time"><i class="fa fa-clock-o"></i><strong> Time: </strong> 18:00h</div>
-              <a href="" class="btn btn-primary">Read more</a>
-            </div>
+  <h1>Nearby Events</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="event-item">
+          <h3>Event #1</h3>
+          <div class="event-description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis veritatis maiores perferendis quas sequi magnam corporis exercitationem natus sint ullam.</p>
           </div>
-
-          <div class="col-sm-4">
-            <div class="event-item">
-              <h3>Event #2</h3>
-              <div class="event-description">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis veritatis maiores perferendis quas sequi magnam corporis exercitationem natus sint ullam.</p>
-              </div>
-              <div class="event-location"><i class="fa fa-map-marker"></i> <strong>Location: </strong> Pivnica Medvedgrad</div>
-              <div class="event-date"><i class="fa fa-calendar"></i><strong> Date: </strong> 20.12.2017.</div>
-              <div class="event-time"><i class="fa fa-clock-o"></i><strong> Time: </strong> 18:00h</div>
-              <a href="" class="btn btn-primary">Read more</a>
-            </div>
-          </div>
-
-          <div class="col-sm-4">
-            <div class="event-item">
-              <h3>Event #3</h3>
-              <div class="event-description">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis veritatis maiores perferendis quas sequi magnam corporis exercitationem natus sint ullam.</p>
-              </div>
-              <div class="event-location"><i class="fa fa-map-marker"></i> <strong>Location: </strong> Pivnica Medvedgrad</div>
-              <div class="event-date"><i class="fa fa-calendar"></i><strong> Date: </strong> 20.12.2017.</div>
-              <div class="event-time"><i class="fa fa-clock-o"></i><strong> Time: </strong> 18:00h</div>
-              <a href="" class="btn btn-primary">Read more</a>
-            </div>
-          </div>          
-        </div>
-        <div class="center all-events-btn">
-          <a class="show-more" href="#">Show all upcoming events</a>
+          <div class="event-location"><i class="fa fa-map-marker"></i> <strong>Location: </strong> Pivnica Medvedgrad</div>
+          <div class="event-date"><i class="fa fa-calendar"></i><strong> Date: </strong> 20.12.2017.</div>
+          <div class="event-time"><i class="fa fa-clock-o"></i><strong> Time: </strong> 18:00h</div>
+          <a href="" class="btn btn-primary">Read more</a>
         </div>
       </div>
-    </section>
+
+      <div class="col-sm-4">
+        <div class="event-item">
+          <h3>Event #2</h3>
+          <div class="event-description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis veritatis maiores perferendis quas sequi magnam corporis exercitationem natus sint ullam.</p>
+          </div>
+          <div class="event-location"><i class="fa fa-map-marker"></i> <strong>Location: </strong> Pivnica Medvedgrad</div>
+          <div class="event-date"><i class="fa fa-calendar"></i><strong> Date: </strong> 20.12.2017.</div>
+          <div class="event-time"><i class="fa fa-clock-o"></i><strong> Time: </strong> 18:00h</div>
+          <a href="" class="btn btn-primary">Read more</a>
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+        <div class="event-item">
+          <h3>Event #3</h3>
+          <div class="event-description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis veritatis maiores perferendis quas sequi magnam corporis exercitationem natus sint ullam.</p>
+          </div>
+          <div class="event-location"><i class="fa fa-map-marker"></i> <strong>Location: </strong> Pivnica Medvedgrad</div>
+          <div class="event-date"><i class="fa fa-calendar"></i><strong> Date: </strong> 20.12.2017.</div>
+          <div class="event-time"><i class="fa fa-clock-o"></i><strong> Time: </strong> 18:00h</div>
+          <a href="" class="btn btn-primary">Read more</a>
+        </div>
+      </div>          
+    </div>
+    <div class="center all-events-btn">
+      <a class="show-more" href="#">Show all upcoming events</a>
+    </div>
+  </div>
+</section>
 
     <section class="history">
       <h1>Finished Events</h1>
@@ -115,11 +115,11 @@
     </section>
     
     <footer>
-      <p>Copyright 2016. QuizUp</p>
-    </footer>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="public/js/bootstrap.min.js"></script>
+  <p>Copyright 2016. QuizUp</p>
+</footer>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="public/js/bootstrap.min.js"></script>
   </body>
 </html>
